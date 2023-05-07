833416214fedaf4903ccb9088513d824a0656622
cenfun <cenfun@gmail.com>
2023-05-07 22:54:59 +0800

    add async deflate

--- scripts/test.js
+++ scripts/test.js
@@ -6,42 +6,47 @@ const {
     compress, decompress, deflate
 } = require('../');
 
-const json = require('../test/test.json');
 
-for (const s of json.list) {
-    const cs = compress(s);
-    console.log(cs);
-    const ds = decompress(cs);
-    if (ds !== s) {
-        throw new Error('compress/decompress error');
+const test = async () => {
+
+    const json = require('../test/test.json');
+
+    for (const s of json.list) {
+        const cs = compress(s);
+        console.log(cs);
+        const ds = decompress(cs);
+        if (ds !== s) {
+            throw new Error('compress/decompress error');
+        }
     }
-}
 
-// for browser
-const jsonStr = JSON.stringify(json);
+    // for browser
+    const jsonStr = JSON.stringify(json);
 
-const dStr = compress(jsonStr);
-const iStr = deflate(jsonStr);
+    const dStr = compress(jsonStr);
+    const iStr = await deflate(jsonStr);
 
-const testData = `window.testData = {
-    raw: ${jsonStr},
-    decompressStr: "${dStr}",
-    inflateStr: "${iStr}"
-};`;
-fs.writeFileSync(path.resolve(__dirname, '../test/test.data.js'), testData);
+    const testData = `window.testData = { raw: ${jsonStr}, decompressStr: "${dStr}", inflateStr: "${iStr}" };`;
+
+    fs.writeFileSync(path.resolve(__dirname, '../test/test.data.js'), testData);
 
 
-// const tempStr = '1234567890';
-// const buf = Buffer.from(tempStr);
+    // const tempStr = '1234567890';
+    // const buf = Buffer.from(tempStr);
 
-// const dRaw = zlib.deflateRawSync(buf);
-// console.log(dRaw);
+    // const dRaw = zlib.deflateRawSync(buf);
+    // console.log(dRaw);
 
-// const d = zlib.deflateSync(buf);
-// console.log(d);
+    // const d = zlib.deflateSync(buf);
+    // console.log(d);
 
-// const dr = new Uint8Array(d.buffer, d.byteOffset + 2, d.length - 6);
-// console.log(Buffer.from(dr));
+    // const dr = new Uint8Array(d.buffer, d.byteOffset + 2, d.length - 6);
+    // console.log(Buffer.from(dr));
 
-// const gz = zlib.gzipSync(buf);
-// console.log(Buffer.from(gz));
+    // const gz = zlib.gzipSync(buf);
+    // console.log(Buffer.from(gz));
+
+};
+
+
+test();
